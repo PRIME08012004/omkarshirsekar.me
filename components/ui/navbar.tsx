96731d9f36c61aa1cf6a04a82206b289a8a01303
Omkar_Shirsekar <iamomkar0007@gmail.com>
2026-06-23 18:38:54 +0530

    fix:navbar

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -5,7 +5,7 @@ import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
 export default function Navbar() {
-  const { theme, setTheme } = useTheme("light");
+  const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
