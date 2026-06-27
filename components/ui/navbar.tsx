12e8b0154188aa6d92e382a7b7d2e85a991343f0
Omkar_Shirsekar <iamomkar0007@gmail.com>
2026-06-27 11:10:21 +0530

    feat:tooltip for navicons

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -42,6 +42,7 @@ export default function Navbar() {
         >
           <path d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577v-2.165c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23A11.509 11.509 0 0112 5.803c1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576C20.566 21.797 24 17.3 24 12c0-6.63-5.37-12-12-12z" />
         </svg>
+         <span className="tooltip">Github</span>
       </NavIcon>
 
       <NavIcon href="https://linkedin.com/in/omkar-shirsekar" label="LinkedIn">
@@ -54,6 +55,7 @@ export default function Navbar() {
         >
           <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
         </svg>
+          <span className="tooltip">LinkedIn</span>
       </NavIcon>
 
       <NavIcon href="https://x.com" label="X / Twitter">
@@ -66,6 +68,7 @@ export default function Navbar() {
         >
           <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.744l7.73-8.835L1.254 2.25H8.08l4.253 5.622 5.912-5.622zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
         </svg>
+          <span className="tooltip">X</span>
       </NavIcon>
 
       {/* Terminal icon — onClick instead of href */}
@@ -88,6 +91,7 @@ export default function Navbar() {
             d="m6.75 7.5 3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0 0 21 18V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v12a2.25 2.25 0 0 0 2.25 2.25Z"
           />
         </svg>
+          <span className="tooltip">Terminal</span>
       </NavIcon>
       <NavIcon href="https://drive.google.com/file/d/1ODWpkzfxffV2Wjn41MzYLJC7PzX6H_s6/view?usp=sharing" label="Resume Link">
         <svg
@@ -104,6 +108,7 @@ export default function Navbar() {
             d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
           />
         </svg>
+          <span className="tooltip">Resume</span>
       </NavIcon>
 
       <span
@@ -242,7 +247,7 @@ function NavIcon({
   };
 
   const sharedClass =
-    "flex items-center justify-center w-[34px] h-[34px] rounded-xl transition-all duration-75";
+    "flex items-center justify-center w-[34px] h-[34px] rounded-xl transition-all duration-75 relative group";
 
   // Button mode (terminal)
   if (onClick) {
