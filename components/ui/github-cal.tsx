43399955ef635f622afc14ff35e4dc1ee9b4de67
Omkar_Shirsekar <iamomkar0007@gmail.com>
2026-06-27 13:42:38 +0530

    add:responsive gitcal for mobile view

--- components/ui/github-cal.tsx
+++ components/ui/github-cal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import dynamic from "next/dynamic";
-
+import { useMediaQuery } from "usehooks-ts";
 const GitHubCalendar = dynamic(
   () =>
     import("react-github-calendar").then(
@@ -11,15 +11,18 @@ const GitHubCalendar = dynamic(
 );
 
 export default function GithubSection() {
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
+const months = isMobile ? 5 : 8;
   return (
-    <div className="flex flex-col items-center gap-4 p-2">
+    <div className="flex flex-col items-center gap-4 p-2 overflow-x-auto">
         <h1>Github Stats</h1>
       <GitHubCalendar
         username="PRIME08012004"
         colorScheme="dark"
         transformData={(contributions) => {
         const sixMonthsAgo = new Date();
-        sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - 8);
+        sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - months);
 
         return contributions.filter(
           (day) => new Date(day.date) >= sixMonthsAgo
